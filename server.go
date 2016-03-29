7cf06c40cb97483fc4df3858aabedbef41d1f386
nordberg <marcusnordberg@live.se>
2016-03-29 22:02:45 +0200

    Get distance matrix but does not parse response body properly yet

--- server.go
+++ server.go
@@ -2,7 +2,10 @@ package main
 
 import (
     "log"
+    "encoding/xml"
     "net/http"
+    "net/url"
+    "io/ioutil"
 )
 
 func main() {
@@ -10,5 +13,48 @@ func main() {
     http.Handle("/", fs)
 
     log.Println("Listening...")
+    /*cityA := "San Francisco"
+    cityB := "Las Vegas"
+    cities := []string{cityA, cityB}
+    log.Printf("Dist from %s to %s is %.2f\n", cityA, cityB,
+      DistCities(cities))*/
     http.ListenAndServe(":8000", nil)
+}
+
+type Response struct {
+  XMLName xml.Name `"xml:response"`
+  Distance float32 `"xml:distance"`
+}
+
+func DistCities(cities []string) float32 {
+
+  u, err := url.Parse("http://mapquestapi.com/directions/v2/routematrix")
+  if err != nil {
+    log.Fatal(err)
+  }
+  q := u.Query()
+  q.Set("key", "vOULz5vyrPWAgcHjh7M9Rd43rFzE3V5Z")
+  q.Set("locations", "json:{locations:[San Francisco, Las Vegas]}")
+  q.Set("outFormat", "xml")
+  u.RawQuery = q.Encode()
+  log.Printf("Api request to %s\n", u)
+  resp, err := http.Get(u.String())
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer resp.Body.Close()
+
+  contents, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  v := &Response{Distance:1.337}
+  xml.Unmarshal(contents, &v)
+  if err != nil {
+    log.Fatal(err)
+  }
+  log.Printf("%s %.2f\n", v.XMLName, v.Distance)
+
+  return 0.01
 }
