ddc726c5fdaf72455575883565551fcb07bcb744
Marcus Nordberg <nordberg@github.com>
2016-03-30 16:33:21 +0200

    Parse cities from POST request

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,6 +11,7 @@ use mount::Mount;
 use std::path::Path;
 use std::io::Read;
 use rustc_serialize::json;
+use rustc_serialize::json::Json;
 
 #[derive(RustcEncodable)]
 struct Distance {
@@ -25,7 +26,11 @@ fn main() {
         let mut payload = String::new();
         res.body.read_to_string(&mut payload).unwrap();
         println!("Response: {}", payload);
-        let dist = Distance {from: "Stockholm".to_string(), to: "Oslo".to_string(), dist: 1337};
+        let data = Json::from_str(&payload).unwrap();
+        let obj = data.as_object().unwrap();
+        let from = obj.get("from").unwrap();
+        let to = obj.get("to").unwrap();
+        let dist = Distance {from: from.to_string(), to: to.to_string(), dist: 1337};
         let payload = json::encode(&dist).unwrap();
         Ok(Response::with(((status::Ok), payload)))
     }
@@ -33,8 +38,8 @@ fn main() {
     let mut mount = Mount::new();
     let path = Path::new("../index.html");
     mount
-        .mount("/citydistance/", dist_cities)
-        .mount("/wlm", Static::new(path));
+        .mount("/", Static::new(path))
+        .mount("/citydistance/", dist_cities);
     println!("Listening on port 8000");
     Iron::new(mount).http("localhost:8000").unwrap();
 }
