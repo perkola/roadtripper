d28a79c7a0faed1d3e46feec6a0ebb8f1c19c8ac
Marcus Nordberg <nordberg@github.com>
2016-03-31 19:30:05 +0200

    Answer to api requests (huge fucking json obj)

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,8 @@ extern crate mount;
 extern crate router;
 extern crate staticfile;
 extern crate rustc_serialize;
+extern crate hyper;
+extern crate dotenv;
 
 use iron::status;
 use iron::prelude::*;
@@ -12,15 +14,13 @@ use std::path::Path;
 use std::io::Read;
 use rustc_serialize::json;
 use rustc_serialize::json::Json;
-
-#[derive(RustcEncodable)]
-struct Distance {
-    from: String,
-    to: String,
-    dist: i32
-}
+use hyper::Client;
+use dotenv::dotenv;
+use std::env;
 
 fn main() {
+
+    dotenv().ok();
 
     fn dist_cities(res: &mut Request) -> IronResult<Response> {
         let mut payload = String::new();
@@ -28,18 +28,31 @@ fn main() {
         println!("Response: {}", payload);
         let data = Json::from_str(&payload).unwrap();
         let obj = data.as_object().unwrap();
-        let from = obj.get("from").unwrap();
-        let to = obj.get("to").unwrap();
-        let dist = Distance {from: from.to_string(), to: to.to_string(), dist: 1337};
-        let payload = json::encode(&dist).unwrap();
+        let mut from = obj.get("from").unwrap().to_string();
+        let mut to = obj.get("to").unwrap().to_string();
+
+        from = (&from[1..from.len()-1]).to_string();
+        to = (&to[1..to.len()-1]).to_string();
+
+        /* TODO: HTTP post to map api */
+        /* TODO: URL ENCODING */
+        let key = env::var("KEY").unwrap();
+        let url_query = format!("http://www.mapquestapi.com/directions/v2/route?from={}&to={}&key={}", from, to, key);
+
+        let client = Client::new();
+        let mut res = client.get(&url_query).send().unwrap();
+        let mut s = String::new();
+        res.read_to_string(&mut s).unwrap();
+        println!("{}", s);
+
+        let payload = json::encode(&s).unwrap();
         Ok(Response::with(((status::Ok), payload)))
     }
 
     let mut mount = Mount::new();
-    let path = Path::new("../index.html");
     mount
-        .mount("/", Static::new(path))
-        .mount("/citydistance/", dist_cities);
+        .mount("/index.html", Static::new(Path::new("../index.html")))
+        .mount("/api/citydistance/", dist_cities);
     println!("Listening on port 8000");
     Iron::new(mount).http("localhost:8000").unwrap();
 }
