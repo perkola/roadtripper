3eee79912d19921861da43ef665f665efffb5f35
Marcus Nordberg <nordberg@github.com>
2016-04-01 22:26:30 +0200

    Add handle for /api/roadtrip/save

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,6 +5,7 @@ extern crate staticfile;
 extern crate rustc_serialize;
 extern crate hyper;
 extern crate dotenv;
+extern crate rand;
 
 use iron::status;
 use iron::prelude::*;
@@ -17,20 +18,24 @@ use rustc_serialize::json::Json;
 use hyper::Client;
 use dotenv::dotenv;
 use std::env;
+use rand::Rng;
 
 fn main() {
 
     dotenv().ok();
 
     fn dist_cities(res: &mut Request) -> IronResult<Response> {
+        /* read payload to a string */
         let mut payload = String::new();
         res.body.read_to_string(&mut payload).unwrap();
+
         println!("Response: {}", payload);
         let data = Json::from_str(&payload).unwrap();
         let obj = data.as_object().unwrap();
         let mut from = obj.get("from").unwrap().to_string();
         let mut to = obj.get("to").unwrap().to_string();
 
+        /* the strings are parsed as "city" rather than city. So we strip "". */
         from = (&from[1..from.len()-1]).to_string();
         to = (&to[1..to.len()-1]).to_string();
 
@@ -49,10 +54,23 @@ fn main() {
         Ok(Response::with(((status::Ok), payload)))
     }
 
+    fn save_roadtrip(res: &mut Request) -> IronResult<Response> {
+        let mut payload = String::new();
+        res.body.read_to_string(&mut payload).unwrap();
+
+        let id = rand::thread_rng()
+            .gen_ascii_chars()
+            .take(10)
+            .collect::<String>();
+
+        Ok(Response::with(((status::Ok, id))))
+    }
+
     let mut mount = Mount::new();
     mount
-        .mount("/index.html", Static::new(Path::new("../index.html")))
-        .mount("/api/citydistance/", dist_cities);
+        .mount("/index.html", Static::new(Path::new("../public/index.html")))
+        .mount("/api/citydistance", dist_cities)
+        .mount("/api/roadtrip/save", save_roadtrip);
     println!("Listening on port 8000");
     Iron::new(mount).http("localhost:8000").unwrap();
 }
