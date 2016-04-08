1ce71f0571f5ce4d4a1a7c80beafe68770b626f9
Marcus Nordberg <nordberg@github.com>
2016-04-08 10:47:35 +0200

    Changed city design

--- front/src/components/Planner.vue
+++ front/src/components/Planner.vue
@@ -5,29 +5,33 @@
       :zoom.sync="zoom",
       :map-type-id.sync="mapType"
   )
-
 div.planner
-  div.roadtrip-cities(v-for="city in cities")
-    div.roadtrip-city
-      div.roadtrip-city__content(v-on:click="showCity")
-        h2 {{ city.name }}
-        h3 Activities
-        div.roadtrip-city__activities(v-for="act in city.activities")
-          {{ act.name }} #[i.roadtrip-city__activities__location at {{ act.location }}]
-        input(form="POST").roadtrip-city__add-activity
-        i.material-icons favorite
-      div.roadtrip-city__transition
-        //i.material-icons.roadtrip-city__transition__icon arrow forward
-        hr.roadtrip-city__transition__travel-line
-        span.roadtrip-city__transition__time 13h37m
+  div.planner-cities
+    div.roadtrip-cities(v-for="city in cities")
+      div.roadtrip-city
+        div.roadtrip-city__content(v-on:click="getCityDistance")
+          div.roadtrip-city__content__title
+            {{ city.name }}
+          div.roadtrip-city__content__content
+            Activities
+            div.roadtrip-city__activities(v-for="act in city.activities")
+              {{ act.name }} #[i.roadtrip-city__activities__location at {{ act.location }}]
+          //
+            input(form="POST").roadtrip-city__add-activity
+            i.material-icons favorite
+        div.roadtrip-city__transition
+          //i.material-icons.roadtrip-city__transition__icon arrow forward
+          span.roadtrip-city__transition__time 13h37m
 
-  div.roadtrip-cities
-    div.roadtrip-city
-      div.roadtrip-city__content(v-on:click="addCity")
-        h2 Add city
-        i.material-icons.md-48 add_circle_outline
+    div.roadtrip-cities
+      div.roadtrip-city
+        div.roadtrip-city__content(v-on:click="addCity")
+          div.roadtrip-city__content__title
+            | Add city
+          div.roadtrip-city__content__add-circle
+            i.material-icons.md-48 add_circle_outline
 
-Timeline
+  Timeline
 
 </template>
 
@@ -52,7 +56,51 @@ export default {
       console.log(e)
     },
     addCity: function() {
-      console.log("Adding a new city")
+      console.log(this.cities)
+      var city = { name: "Stockholm", activities: [{name: "Roll", location: "KTH"}]}
+      this.cities.push(city)
+    },
+    getCityDistance: function() {
+      makeCorsRequest()
+      function createCORSRequest(method, url) {
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+          // XHR for Chrome/Firefox/Opera/Safari.
+          xhr.open(method, url, true);
+        } else if (typeof XDomainRequest != "undefined") {
+          // XDomainRequest for IE.
+          xhr = new XDomainRequest();
+          xhr.open(method, url);
+        } else {
+          // CORS not supported.
+          xhr = null;
+        }
+        return xhr;
+      }
+
+      function makeCorsRequest() {
+        // All HTML5 Rocks properties support CORS.
+        var url = 'http://localhost:8000/api/citydistance?from=dallas&to=san%20francisco';
+
+        var xhr = createCORSRequest('GET', url);
+        if (!xhr) {
+          alert('CORS not supported');
+          return;
+        }
+
+        // Response handlers.
+        xhr.onload = function() {
+          var text = xhr.responseText;
+          alert('Response from CORS request to ' + url);
+        };
+
+        xhr.onerror = function() {
+          alert('Woops, there was an error making the request.');
+        };
+
+        xhr.send();
+      }
+
     }
   },
   components: {
